8db6a362abb4f802799f8d91d5d0dd0354f95da2
Tran Phat Trien <trientranphat@gmail.com>
2022-04-23 02:18:03 +0700

    Upload

---
+++ client/src/api.js
@@ -0,0 +1,30 @@
+import axios from "axios";
+const url = "/api/post";
+
+export default class API {
+    //to get all the posts from the server
+    static async getAllPost() {
+            const res = await axios.get(url);
+            return res.data;
+        }
+        //to get single posts by id
+    static async getPostByID(id) {
+            const res = await axios.get(`${url}/${id}`);
+            return res.data;
+        }
+        //to insert post into database
+    static async addPost(post) {
+            const res = await axios.post(url, post);
+            return res.data;
+        }
+        //to update post into database
+    static async addPost(id, post) {
+            const res = await axios.patch(`${url}/${id}`);
+            return res.data;
+        }
+        //to delete a post
+    static async deletePost(id) {
+        const res = await axios.delete(`${url}/${id}`);
+        return res.data;
+    }
+}
---
+++ client/src/router/index.js
@@ -0,0 +1,41 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import AddPost from '../views/AddPost.vue'
+import Post from '../views/Post.vue'
+
+Vue.use(VueRouter)
+
+const routes = [{
+        path: '/',
+        name: 'home',
+        component: HomeView,
+    },
+    {
+        path: '/add-post',
+        name: 'add-post',
+        component: AddPost,
+    },
+    {
+        path: '/post/:id',
+        name: 'post',
+        component: Post,
+    },
+    {
+        path: '/about',
+        name: 'about',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+            import ( /* webpackChunkName: "about" */ '../views/AboutView.vue')
+    }
+]
+
+const router = new VueRouter({
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
+})
+
+export default router
